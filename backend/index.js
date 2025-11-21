639e5db97289bc67ccab32ed089fd245546a28a9
Celoma <salouet.clement049@gmail.com>
2025-11-21 15:07:14 +0100

    redeploy

--- backend/index.js
+++ backend/index.js
@@ -3,14 +3,16 @@ import cors from 'cors';
 
 const app = express();
 
+// Autoriser le front Vercel
 app.use(cors({
-  origin: 'https://<TON_FRONTEND_VERCEL_URL>'
+  origin: 'https://oscar-deploiement.vercel.app'
 }));
 
 app.get('/api/hello', (req, res) => {
-  res.json({ message: 'Hello from backend!!!' });
+  res.json({ message: 'Hello from backend!' });
 });
 
+// Utiliser le port fourni par Railway
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
   console.log(`Backend listening on port ${port}`);
