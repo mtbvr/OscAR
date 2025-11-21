46cc7cdad025c30012582c94a0d0206d9384576e
Celoma <salouet.clement049@gmail.com>
2025-11-21 15:50:29 +0100

    il a menti

--- backend/eslint.config.js
+++ backend/eslint.config.js
@@ -4,6 +4,22 @@ import pluginReact from "eslint-plugin-react";
 import { defineConfig } from "eslint/config";
 
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs,jsx}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.browser } },
+  {
+  "env": {
+    "browser": false,
+    "node": true,
+    "es2021": true
+  },
+  "extends": ["eslint:recommended"],
+  "parserOptions": {
+    "ecmaVersion": "latest",
+    "sourceType": "module"
+  },
+  "plugins": ["react"],
+  "rules": {}
+ }
+,
   pluginReact.configs.flat.recommended,
 ]);
+
+
