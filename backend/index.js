cb5c1bd7b65ad1c540853beaaac1ec103d22698f
Celoma <salouet.clement049@gmail.com>
2025-11-21 16:34:18 +0100

    Nouvelle méthode

--- backend/index.js
+++ backend/index.js
@@ -9,7 +9,7 @@ app.use(cors({
 }));
 
 app.get('/api/hello', (req, res) => {
-  res.json({ message: 'Heddlldddod ffddrosfffdme fbackvcbvcbddend!' });
+  res.json({ message: 'Heddlldddod ffddrosfffdme gfdfbackvcbvcbddend!' });
 });
 
 // Utiliser le port fourni par Railway
