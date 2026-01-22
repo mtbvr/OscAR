c875eeb513ca9bf5af67775d91aa055e01304711
mateo <mateobouvier10@gmail.com>
2026-01-22 17:08:42 +0100

    [FRONT-BACK] - Liaison FrontBack passage du front en TS update docker-compose

--- frontend-desktop/src/pages/authentification/index.tsx
+++ frontend-desktop/src/pages/authentification/index.tsx
@@ -1,5 +1,6 @@
-import React from "react";
 import DynamicForm from "../../common/components/dynamic_form/DynamicForm.jsx";
+import { CreateUserDto } from "../../api/models/users/AddUserDto.js";
+import { addUser } from "../../api/services/users.api.js";
 
 export default function Authentification() {
   const fields = [
@@ -8,8 +9,20 @@ export default function Authentification() {
     { name: "username", label: "Nom d'utilisateur", type: "text", required: true },
   ];
 
-  const handleSubmit = (values) => {
+  const handleSubmit = async (values: CreateUserDto) => {
     console.log("Form values:", values);
+
+    if (!values.email || !values.username || !values.password) {
+      console.error("Missing fields");
+      return;
+    }
+
+    try {
+      const newUser = await addUser(values);
+      console.log("Succes:", newUser);
+    } catch (err) {
+      console.error("Erreur:", err);
+    }
   };
 
   return (
--- frontend-desktop/src/pages/home/index.tsx
+++ frontend-desktop/src/pages/home/index.tsx
@@ -1,5 +1,5 @@
 export default function Home() {
   return (
-    <p>Lootopia !</p>
+    <span>Lootopia !</span>
   );
 }
