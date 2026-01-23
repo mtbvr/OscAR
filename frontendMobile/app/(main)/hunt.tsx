a35343f8c47da6d1bd7a97cb1c1d067c07e3f773
gabinfremont <gabin.fremont@free.fr>
2026-01-23 17:32:37 +0100

    GFRE - Add social friend leaderboard NOT CONNECTED)

--- frontendMobile/app/(main)/hunt.tsx
+++ frontendMobile/app/(main)/hunt.tsx
@@ -1,18 +1,16 @@
 import React from 'react';
-import { View, Text, TouchableOpacity } from 'react-native';
+import { View, Text } from 'react-native';
 import { theme, globalStyles } from '../../constants/theme';
-import { LinearGradient } from 'expo-linear-gradient';
-import { SvgUri } from 'react-native-svg';
-import { Asset } from 'expo-asset';
-import { useRouter } from 'expo-router';
+import PlaceholderNotConnected from '../../components/placeholder-not-connected';
 import { useAuth } from '../../context/AuthContext';
+import { useRouter } from 'expo-router';
+import { Asset } from 'expo-asset';
+import { SvgUri } from 'react-native-svg';
 
 // Icon mapping
 const ICONS = {
-    "clock.svg": require('../../assets/icon/clock.svg'),
-    "check.svg": require('../../assets/icon/check.svg'),
-    "target.svg": require('../../assets/icon/target.svg'),
     "target-larger.svg": require('../../assets/icon/target-larger.svg'),
+    "check.svg": require('../../assets/icon/check.svg'),
 } as const;
 
 // Define the type for the keys of ICONS
@@ -29,32 +27,25 @@ function getIconUri(iconName: IconName): string {
 }
 
 // HuntSection component for each section (Current Hunts, Completed Hunts)
-const HuntSection = ({ title, icon, placeholderIcon, placeholderMessage, buttonText, isAuthenticated }: { title: string; icon: IconName; placeholderIcon: IconName; placeholderMessage: string; buttonText: string; isAuthenticated: boolean }) => {
+const HuntSection = ({ title, icon, iconColor, placeholderIcon, placeholderMessage, buttonText, isAuthenticated }: { title: string; icon: IconName; iconColor: string; placeholderIcon: string; placeholderMessage: string; buttonText: string; isAuthenticated: boolean }) => {
     const router = useRouter();
-
+    
     return (
         <View style={{ flexDirection: 'column', marginTop: theme.SPACING.medium }}>
-            <View style={{ flexDirection: 'row', marginBottom: theme.SPACING.medium }}>
-                <SvgUri uri={getIconUri(icon)} width={20} height={20} color={icon === "check.svg" ? theme.COLORS.success : theme.COLORS.primary} style={{ marginRight: 3 }} />
+            <View style={{ flexDirection: 'row', marginBottom: theme.SPACING.medium, alignItems: 'center' }}>
+                <SvgUri uri={getIconUri(icon)} width={25} height={25} color={iconColor} />
                 <Text style={{ ...globalStyles.subtitle, fontSize: theme.FONT_SIZES.text, marginLeft: theme.SPACING.small }}>{title}</Text>
             </View>
-            <View style={{ alignItems: 'center', justifyContent: 'center', borderRadius: 12, backgroundColor: '#ededed', paddingHorizontal: theme.SPACING.medium, paddingVertical: theme.SPACING.large, borderColor: '#C2C2C2', borderWidth: 1 }}>
-                {isAuthenticated ? (
-                    <Text style={{ textAlign: 'center', color: theme.COLORS.textSecondary }}>Logique d'affichage des chasses en fonction de la connexion ici</Text>
-                ) : (
-                    <>
-                        <SvgUri uri={getIconUri(placeholderIcon)} width={50} height={50} color={"#cccccc"} style={{ marginBottom: theme.SPACING.medium }} />
-                        <Text style={{ textAlign: 'center', color: '#a0a0a0', marginBottom: theme.SPACING.medium }}>{placeholderMessage}</Text>
-                        <TouchableOpacity style={[theme.BUTTON_STYLES.default, { width: '100%' }]} onPress={() => router.push('/connection')}>
-                            <LinearGradient colors={[theme.COLORS.primary, theme.COLORS.secondary]} start={{ x: 0, y: 0 }} end={{ x: 1, y: 0 }} style={[theme.BUTTON_STYLES.default, { width: '100%' }]}>
-                                <View style={{ alignItems: 'center' }}>
-                                    <Text style={{ color: '#FFFFFF', fontWeight: '700' }}>{buttonText}</Text>
-                                </View>
-                            </LinearGradient>
-                        </TouchableOpacity>
-                    </>
-                )}
-            </View>
+            {isAuthenticated ? (
+                <Text style={{ textAlign: 'center', color: theme.COLORS.textSecondary }}>Logique d'affichage des chasses en fonction de la connexion ici</Text>
+            ) : (
+                <PlaceholderNotConnected
+                    icon={placeholderIcon as any}
+                    message={placeholderMessage}
+                    buttonText={buttonText}
+                    onPress={() => router.push('/connection')}
+                />
+            )}
         </View>
     );
 };
@@ -68,18 +59,20 @@ export default function HuntScreen() {
             <Text style={{ ...globalStyles.subtitle, fontWeight: '800' }}>Mes Chasses</Text>
             <HuntSection
                 title="Chasses en cours (0)"
-                icon="clock.svg"
+                icon="target-larger.svg"
+                iconColor={theme.COLORS.primary}
                 placeholderIcon="target-larger.svg"
                 placeholderMessage="Connectez-vous pour pouvoir voir vos chasses en cours !"
-                buttonText="Se connecter"
+                buttonText="Se connecter →"
                 isAuthenticated={isAuthenticated}
             />
             <HuntSection
                 title="Chasses complétées (0)"
                 icon="check.svg"
+                iconColor={theme.COLORS.success}
                 placeholderIcon="target-larger.svg"
                 placeholderMessage="Connectez-vous pour pouvoir enregistrer votre progression !"
-                buttonText="Se connecter"
+                buttonText="Se connecter →"
                 isAuthenticated={isAuthenticated}
             />
         </View>
--- frontendMobile/app/(main)/social.tsx
+++ frontendMobile/app/(main)/social.tsx
@@ -1,14 +1,52 @@
 import React from 'react';
 import { View, Text } from 'react-native';
-import { theme, globalStyles } from '../../constants/theme';
+import { useAuth } from '../../context/AuthContext';
+import { theme } from '../../constants/theme';
+import PlaceholderNotConnected from '../../components/placeholder-not-connected';
+import { router } from 'expo-router';
+import { Asset } from 'expo-asset';
+import { SvgUri } from 'react-native-svg';
+
+// Icon mapping
+const ICONS = {
+    "trophy.svg": require('../../assets/icon/trophy.svg'),
+} as const;
+
+// Define the type for the keys of ICONS
+type IconName = keyof typeof ICONS;
+
+// Function to get the URI of the SVG icon
+function getIconUri(iconName: IconName): string {
+    const iconSource = ICONS[iconName];
+    if (!iconSource) {
+        console.error(`Icon "${iconName}" not found in ICONS mapping.`);
+        return '';
+    }
+    return Asset.fromModule(iconSource).uri || '';
+}
 
 // Social screen
-
 export default function SocialScreen() {
+    const { isAuthenticated } = useAuth();
+
     return (
-        <View style={[theme.CONTAINER_STYLES.center, theme.CONTAINER_STYLES.padded]}>
-            <Text style={globalStyles.subtitle}>Social</Text>
-            <Text style={globalStyles.smallText}>Interactions sociales à venir.</Text>
+        <View style={{ backgroundColor: theme.COLORS.background, paddingTop: theme.SPACING.large, paddingLeft: theme.SPACING.medium, paddingRight: theme.SPACING.medium, alignSelf: 'center', width: '100%' }}>
+            <View style={{ borderRadius: 8, borderColor: theme.COLORS.border, borderWidth: 1, paddingVertical: theme.SPACING.large, paddingHorizontal: theme.SPACING.medium }}>
+                <View style={{ flexDirection: 'row', marginBottom: theme.SPACING.large, width: '100%' }}>
+                    <SvgUri uri={getIconUri("trophy.svg")} width={30} height={30} color={theme.COLORS.secondary} />
+                    <Text style={{ marginLeft: theme.SPACING.small, fontSize: theme.FONT_SIZES.subtitle, fontWeight: '700', color: theme.COLORS.textPrimary }}>Classement des amis</Text>
+                </View>
+                {!isAuthenticated ? (
+                    <PlaceholderNotConnected
+                        icon="group.svg"
+                        message="Connectez-vous pour pouvoir ajouter des amis et voir leur progression !"
+                        buttonText="Se connecter →"
+                        onPress={() => router.push('/connection')}
+                    />
+                ) : (
+                    <Text style={{ fontSize: theme.FONT_SIZES.text, color: theme.COLORS.textSecondary, textAlign: 'center' }}>Liste des amis à venir</Text>
+                )}
+            </View>
         </View>
     );
 }
