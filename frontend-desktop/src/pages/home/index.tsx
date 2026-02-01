6cef82afc1b0d2f79356914819861810b39dcab4
mateo <mateobouvier10@gmail.com>
2026-02-01 19:51:12 +0100

    Skeletton form front

--- frontend-desktop/src/pages/home/index.tsx
+++ frontend-desktop/src/pages/home/index.tsx
@@ -1,9 +1,11 @@
 import { getAllDifficulty } from "../../api/services/difficulty.api";
+import { getAllIndexByHunt } from "../../api/services/index.api";
 import DynamicForm from "../../common/components/dynamic_form/DynamicForm";
 
 export default function Home() {
 
   const difficultyData = async() => {getAllDifficulty}
+  const indexByHuntData = async(hunt_id: string) => {getAllIndexByHunt(hunt_id)}
 
   const addHuntFields = 
     [
@@ -12,11 +14,36 @@ export default function Home() {
       { name: "points", label: "Points de la chasse", type: "number", required: true },
       { name: "latitude", label: "Latitude de la chasse", type: "number", required: false},
       { name: "longitude", label: "Longitude de la chasse", type: "number", required: false},
-      //TODO Create select with all difficulty options
+      //TODO Create select with all difficulty in options
+    ]
+
+  const addStepFields =
+    [
+      { name: "title", label: "Titre de la chasse", type: "text", required: true },
+      { name: "description", label: "Description de la chasse", type: "text", required: true },
+      { name: "points", label: "Points de la chasse", type: "number", required: true },
+      { name: "latitude", label: "Latitude de la chasse", type: "number", required: false},
+      { name: "longitude", label: "Longitude de la chasse", type: "number", required: false},
+      //TODO Create select with all different hunts in options
+      //TODO Create Boolean button would you add this step on step group ? Yes : Show all hunt index + button add new Index / No : Don't put index_id on body
+    ]
+
+  const addIndexFields =
+    [
+      { name: "name", label: "Nom de l'index", type: "text", required: true },
+      //TODO don't forget to get the hunt_id and send with the request body
     ]
 
   const handleAddHunt = async() => {
+    console.log("create hunt")
+  }
 
+  const handleAddStep = async() => {
+    console.log("create step")
+  }
+
+  const handleAddIndex = async() => {
+    console.log("create index")
   }
 
   return (
@@ -28,10 +55,30 @@ export default function Home() {
             Ajouter une chasse
           </h2>
           <DynamicForm
-                  fields={addHuntFields}
-                  onSubmit={handleAddHunt}
-                  submitLabel="Envoyer"
-            />
+            fields={addHuntFields}
+            onSubmit={handleAddHunt}
+            submitLabel="Envoyer"
+          />
+        </section>
+
+        <section>
+          <h2>
+            Ajouter un Index à une chasse
+          </h2>
+          <DynamicForm
+            fields={addIndexFields}
+            onSubmit={handleAddIndex}
+            submitLabel="Envoyer"
+          />
+        </section>
+
+        <section>
+          <h2>Ajouter une étape à une chasse</h2>
+          <DynamicForm
+            fields={addStepFields}
+            onSubmit={handleAddStep}
+            submitLabel="Envoyer"
+          />
         </section>
 
     </>
